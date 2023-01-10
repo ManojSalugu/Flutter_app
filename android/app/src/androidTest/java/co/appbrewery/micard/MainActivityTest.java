bfc0fab9a65a64928f73461665d8e7e587675474
ManojSalugu <Manojkumar.s@cawstudios.com>
2023-01-11 00:37:28 +0530

    Setup fix for firebase tests

--- android/app/src/androidTest/java/co/appbrewery/micard/MainActivityTest.java
+++ android/app/src/androidTest/java/co/appbrewery/micard/MainActivityTest.java
@@ -5,7 +5,7 @@ import dev.flutter.plugins.integration_test.FlutterTestRunner;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 // Import MainActivity from YOUR package
-import coappbrewery.micard.MainActivityTest;
+import co.appbrewery.micard.MainActivityTest;
 
 @RunWith(FlutterTestRunner.class)
 public class MainActivityTest {
